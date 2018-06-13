b0d9b56a8f4212bf1b642c33a133b2049caad143
priyasood1505 <priya.sood@in.ey.com>
2018-06-14 00:10:34 +0530

    minor changes

--- web-apps/public/javascripts/index.js
+++ web-apps/public/javascripts/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
   var timeData = [],
     temperatureData = [],
-    vibrationData = [];
-    currentData = [];
+    
   var data = {
     labels: timeData,
     datasets: [
@@ -42,83 +41,6 @@ $(document).ready(function () {
     ]
   }
 
-  var data2 = {
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'Vibration',
-        yAxisID: 'Vibration',
-        borderColor: "rgba(24, 120, 240, 1)",
-        pointBoarderColor: "rgba(24, 120, 240, 1)",
-        backgroundColor: "rgba(24, 120, 240, 0.4)",
-        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
-        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: vibrationData
-      },
-      {
-  		data: Array.apply(null, new Array(10000)).map(Number.prototype.valueOf, 12),
-  		fill: false,
-  		label: 'Upper Control Limit',
-  		radius: 0,
-  		borderColor: "rgba(255, 0, 0, 1)",
-        pointBoarderColor: "rgba(255, 0, 0, 1)",
-  		backgroundColor: "rgba(255, 0, 0, 0.4)",
-  		pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 0, 0, 1)",
-	},
-    {
-  		data: Array.apply(null, new Array(10000)).map(Number.prototype.valueOf, 7),
-  		fill: false,
-  		label: 'Lower Control Limit',
-  		radius: 0,
-  		borderColor: "rgba(255, 140, 0, 1)",
-        pointBoarderColor: "rgba(255, 140, 0, 1)",
-  		backgroundColor: "rgba(255, 140, 0, 0.4)",
-  		pointHoverBackgroundColor: "rgba(255, 140, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 140, 0, 1)",
-	}
-    ]
-  }
-
-  var data3 = {
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'Current',
-        yAxisID: 'Current',
-        borderColor: "rgba(30, 129, 68, 1)",
-        pointBoarderColor: "rgba(30, 129, 68, 1)",
-        backgroundColor: "rgba(30, 129, 68, 0.4)",
-        pointHoverBackgroundColor: "rgba(30, 129, 168, 1)",
-        pointHoverBorderColor: "rgba(30, 129, 68, 1)",
-        data: currentData
-      },
-      {
-  		data: Array.apply(null, new Array(10000)).map(Number.prototype.valueOf, 33),
-  		fill: false,
-  		label: 'Upper Control Limit',
-  		radius: 0,
-  		borderColor: "rgba(255, 0, 0, 1)",
-        pointBoarderColor: "rgba(255, 0, 0, 1)",
-  		backgroundColor: "rgba(255, 0, 0, 0.4)",
-  		pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 0, 0, 1)",
-	},
-    {
-  		data: Array.apply(null, new Array(10000)).map(Number.prototype.valueOf, 27),
-  		fill: false,
-  		label: 'Lower Control Limit',
-  		radius: 0,
-  		borderColor: "rgba(255, 140, 0, 1)",
-        pointBoarderColor: "rgba(255, 140, 0, 1)",
-  		backgroundColor: "rgba(255, 140, 0, 0.4)",
-  		pointHoverBackgroundColor: "rgba(255, 140, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 140, 0, 1)",
-	}
-    ]
-  }
   
   var basicOption = {
     title: {
@@ -139,47 +61,8 @@ $(document).ready(function () {
     }
   }
 
-var basicOption2 = {
-    title: {
-      display: true,
-      text: 'Vibrations Real-time Data',
-      fontSize: 36
-    },
-    scales: {
-      yAxes: [{
-        id: 'Vibration',
-        type: 'linear',
-        scaleLabel: {
-          labelString: 'Vibration (no./sec)',
-          display: true
-        },
-        position: 'left',
-      }]
-    }
-  }
-
-var basicOption3 = {
-    title: {
-      display: true,
-      text: 'Current Real-time Data',
-      fontSize: 36
-    },
-    scales: {
-      yAxes: [{
-        id: 'Current',
-        type: 'linear',
-        scaleLabel: {
-          labelString: 'Current (A)',
-          display: true
-        },
-        position: 'left',
-      }]
-    }
-  }
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
-  var ctx2 = document.getElementById("myChart2").getContext("2d");
-  var ctx3 = document.getElementById("myChart3").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
@@ -187,17 +70,6 @@ var basicOption3 = {
     options: basicOption
   });
 
-  var myLineChart2 = new Chart(ctx2, {
-    type: 'line',
-    data: data2,
-    options: basicOption2
-  });
-
-  var myLineChart3 = new Chart(ctx3, {
-    type: 'line',
-    data: data3,
-    options: basicOption3
-  });
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
@@ -221,23 +93,8 @@ var basicOption3 = {
         temperatureData.shift();
       }
 
-      if (obj.vibration) {
-        vibrationData.push(obj.vibration);
-      }
-      if (vibrationData.length > maxLen) {
-        vibrationData.shift();
-      }
-		
-      if (obj.current) {
-      	currentData.push(obj.current);
-      }
-      if (currentData.length > maxLen) {
-      	currentData.shift();
-      }
       
       myLineChart.update();
-      myLineChart2.update();
-      myLineChart3.update();
     } catch (err) {
       console.error(err);
     }
